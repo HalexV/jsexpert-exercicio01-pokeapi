8a2107588b55373cea16e3afce3c29c19e4e82e9
Halex Viotto Gomes <halexx002@gmail.com>
2022-01-15 10:52:08 -0400

    feat: should throw when an id is not passed

--- src/repositories/pokemonsRepository.js
+++ src/repositories/pokemonsRepository.js
@@ -4,6 +4,8 @@ import utils from "../utils/utils.js";
 
 class PokemonsRepository {
   async findById(id) {
+    if(!id) throw new Error('An id must be informed')
+    
     const URL = `https://pokeapi.co/api/v2/pokemon/${id}`
 
     const { data, status } = await utils.makeGetRequest(URL)
