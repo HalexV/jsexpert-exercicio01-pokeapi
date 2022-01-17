cb06389b0f82026816d189f2cb094986b55cd9be
Halex Viotto Gomes <halexx002@gmail.com>
2022-01-17 13:34:49 -0400

    feat: should call getThreeUniqueRandomIds with 493

--- src/services/getRandomPokemonTeamService.js
+++ src/services/getRandomPokemonTeamService.js
@@ -1,12 +1,28 @@
 'use strict';
 
+import utils from "../utils/utils.js";
+
 class GetRandomPokemonTeamService {
+  
   constructor({ pokemonsRepository }) {
     this.pokemonsRepository = pokemonsRepository
   }
   
+  // Todos os pokémons possuem no mínimo três moves?
   async execute() {
+    const END_RANGE = 493
 
+    const idsArray = utils.getThreeUniqueRandomIds(END_RANGE)
+
+    // Obter as informações do pokémon pelo id
+
+    // Verificar se o pokemon tem no mínimo 3 moves
+
+    // Montar o time de pokemon com os names e moves
+
+    // Retornar o time
+
+    return null
   }
 }
 
