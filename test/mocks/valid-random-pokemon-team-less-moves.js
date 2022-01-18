44384bb1debde571d77f20260a92d65afd8846b3
Halex Viotto Gomes <halexx002@gmail.com>
2022-01-18 09:52:34 -0400

    chore: add mock files

---
+++ test/mocks/valid-random-pokemon-team-less-moves.js
@@ -0,0 +1,23 @@
+export const validRandomPokemonTeamLessMovesMock = [
+  {
+    name: 'bulbasaur',
+    moves: [
+      'razor-wind',
+      'swords-dance',
+      'cut'
+    ]
+  },
+  {
+    name: 'ivysaur',
+    moves: [
+      'swords-dance',
+      'cut'
+    ]
+  },
+  {
+    name: 'venusaur',
+    moves: [
+      'swords-dance',
+    ]
+  },
+]
